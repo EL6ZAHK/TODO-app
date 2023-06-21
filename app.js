fc578a7f6cc3796ac74db3589778283186bb84bd
Eyob <eyoblema19926@gmail.com>
2023-06-21 18:57:57 +0300

    created a controler module for todo and initailize the route

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
-
+var todoController = requre('./controllers/todoController');
 var app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
 
+
+todoController(app);
 
 app.listen(3000);
 console.log('you are listening to port 3000');
