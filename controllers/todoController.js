ccad2803019fd39ebdab480d4d0ca138371d9900
Eyob <eyoblema19926@gmail.com>
2023-06-21 23:24:57 +0300

    add item and delete item working with json

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,23 @@
+var bodyParser = require('body-parser');
+var data = [{item:'Get milk'}, {item:'Walk Cat'},{item:'Kick some cats'}]
 module.exports = function(app){
 
+    var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
     app.get('/todo', function(req, res) {
-        res.render('todo');
+        res.render('todo', {todos:data} );
     });
 
-    app.post('/todo', function(req,res){
-
+    app.post('/todo',urlencodedParser, function(req,res){
+       data.push(req.body);
+       res.json(data);
     });
 
-    app.delete('/todo', function(req,res){
+    app.delete('/todo/:item', function(req,res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !==  req.params.item;
+        });
 
+        res.json(data);
     });
 };
